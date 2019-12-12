5ead00879167c6c5aa9dd7ca0d449274127df7c3
HTML Developer <45798087+NataliaZinovskaya@users.noreply.github.com>
2019-12-12 18:06:56 +0300

    slider, counter, section work steps

--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,43 @@ $(document).ready(function () {
         }, 700, 'linear');
     });
 
+    $('.team-slider-content').slick({
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        asNavFor: '.team-description-slider',
+        focusOnSelect: true,
+        cssEase: 'linear'
+    });
+
+    $('.team-description-slider').slick({
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.team-slider-content',
+        cssEase: 'linear'
+    });
+
+
+    var show = true;
+    var countbox = ".statistics-box__numbers-counter";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false;
+        var w_top = $(window).scrollTop();
+        var e_top = $(countbox).offset().top;
+        var w_height = $(window).height();
+        var d_height = $(document).height();
+        var e_height = $(countbox).outerHeight();
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.statistics-box__numbers-counter').css('opacity', '1');
+            $('.statistics-box__numbers-counter').spincrement({
+                thousandSeparator: "",
+                duration: 1200
+            });
+
+            show = false;
+        }
+    });
+
 });
